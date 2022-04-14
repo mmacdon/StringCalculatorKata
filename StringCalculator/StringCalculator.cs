8c7123bec5c3d676dc34188ac7120d8dc2ceebb2
Mary Macdonald <mary.macdonald@mims.com>
2022-04-15 09:27:12 +1000

    Add project files.

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata;
+public class StringCalculator
+{
+    public int Add(string numbers)
+    {
+        if (string.IsNullOrEmpty(numbers)) { return 0; }
+
+        return numbers.Split(',').Select(n => int.Parse(n)).Sum();
+    }
+}
---
+++ StringCalculator/StringCalculatorAdd.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+namespace StringCalculatorKata;
+public class StringCalculatorAdd
+{
+    private readonly StringCalculator sut = new();
+
+    [Fact]
+    public void EmptyStringReturns0()
+    {
+        var result = sut.Add("");
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("2", 2)]
+    [InlineData("100", 100)]
+    public void GivenStringWithOneNumberReturnsNumber(string numbers, int expectedResult)
+    {
+        var result = sut.Add(numbers);
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Theory]
+    [InlineData("1,2", 3)]
+    [InlineData("20,2", 22)]
+    [InlineData("100,-1", 99)]
+    public void GivenStringWithTwoCommaSeparatedNumbersReturnsSumOfNumbers(string numbers, int expectedResult)
+    {
+        var result = sut.Add(numbers);
+        Assert.Equal(expectedResult, result);
+    }
+}
